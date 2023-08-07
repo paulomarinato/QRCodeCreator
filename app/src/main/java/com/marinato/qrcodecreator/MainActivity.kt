99aa6baca9aa542f643ddad3c48a02c6e1ac1d1c
paulomarinato <marinatopaulo@gmail.com>
2023-08-06 23:59:41 -0300

    config

--- app/src/main/java/com/marinato/qrcodecreator/MainActivity.kt
+++ app/src/main/java/com/marinato/qrcodecreator/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.marinato.qrcodecreator.ui.theme.QRCodeCreatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +21,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    Greeting("Android")
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QRCodeCreatorTheme {
+        Greeting("Android")
     }
 }
