784c8ca65c1471232c576eec10e57df517570684
paulomarinato <marinatopaulo@gmail.com>
2023-08-07 00:08:16 -0300

    Config Colum

--- app/src/main/java/com/marinato/qrcodecreator/QRCodeAPP.kt
+++ app/src/main/java/com/marinato/qrcodecreator/QRCodeAPP.kt
@@ -1,17 +1,21 @@
 package com.marinato.qrcodecreator
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.marinato.qrcodecreator.ui.theme.QRCodeCreatorTheme
 
 @Composable
 fun QRCodeApp(){
-    Text("Olá Mundo")
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        
+    }
 }
 
 @Preview(showBackground = true)
