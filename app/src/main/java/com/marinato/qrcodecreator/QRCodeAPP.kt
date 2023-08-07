23d9a9423f49a424d8f8b58def8b450c6d63edc1
paulomarinato <marinatopaulo@gmail.com>
2023-08-06 23:28:30 -0300

    Criado Função QRCodeAppPreview

---
+++ app/src/main/java/com/marinato/qrcodecreator/QRCodeAPP.kt
@@ -0,0 +1,14 @@
+package com.marinato.qrcodecreator
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.marinato.qrcodecreator.ui.theme.QRCodeCreatorTheme
+
+@Preview(showBackground = true)
+@Composable
+
+fun QRCodeAppPreview(){
+    QRCodeCreatorTheme() {
+        
+    }
+}
