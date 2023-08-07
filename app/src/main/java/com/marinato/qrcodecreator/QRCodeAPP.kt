8ec3a169647412e338e96545d54666671ee16ab7
paulomarinato <marinatopaulo@gmail.com>
2023-08-07 00:06:14 -0300

    resolvido bug de SDK, indisponibilidade de apresentar layout

--- app/src/main/java/com/marinato/qrcodecreator/QRCodeAPP.kt
+++ app/src/main/java/com/marinato/qrcodecreator/QRCodeAPP.kt
@@ -1,2 +1,28 @@
 package com.marinato.qrcodecreator
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.marinato.qrcodecreator.ui.theme.QRCodeCreatorTheme
+
+@Composable
+fun QRCodeApp(){
+    Text("Olá Mundo")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QRCodeAppPreview(){
+    QRCodeCreatorTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            QRCodeApp()
+        }
+    }
+}
